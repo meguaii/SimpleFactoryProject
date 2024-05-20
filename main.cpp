41f3190fa60285e5337116266a95698c0b12e869
meguaii <uriydronov2006@gmail.com>
2024-05-20 09:26:14 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "SimpleFactory.h"
+
+int main() {
+    std::cout << "Simple Factory Example" << std::endl;
+    // Пример использования вашего кода
+    // YourClass example;
+    // example.yourMethod();
+    return 0;
+}
